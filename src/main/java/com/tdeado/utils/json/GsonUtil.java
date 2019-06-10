629662b80066d1f18b5b395473b99ad016300134
杨哲 <2850755733@qq.com>
2019-06-10 15:53:35 +0800

    修复 gson转换时LocalDateTime的解析异常

--- src/main/java/com/tdeado/utils/json/GsonUtil.java
+++ src/main/java/com/tdeado/utils/json/GsonUtil.java
@@ -1,10 +1,13 @@
 package com.tdeado.utils.json;
 
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +16,32 @@ public class GsonUtil {
     private static GsonUtil ourInstance = new GsonUtil();
 
 
-    private static Gson gson = new Gson();
+    private static Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+                @Override
+                public JsonElement serialize(LocalDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+                    return new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                }
+
+            }
+    ).registerTypeAdapter(LocalDate.class, new JsonSerializer<LocalDate>() {
+        @Override
+        public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        }
+    }).registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+                @Override
+                public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                    String datetime = json.getAsJsonPrimitive().getAsString();
+                    return LocalDateTime.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                }
+            }).registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+                @Override
+                public LocalDate deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                    String datetime = json.getAsJsonPrimitive().getAsString();
+                    return LocalDate.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                }
+            }).create();
+
     public static GsonUtil getInstance() {
         return ourInstance;
     }
@@ -24,78 +52,92 @@ public class GsonUtil {
 
     /**
      * 参数转json
+     *
      * @param string
      * @return
      */
-    public static String paramTojson(String string){
-        Map<String,String> stringMap = new HashMap<String, String>();
+    public static String paramTojson(String string) {
+        Map<String, String> stringMap = new HashMap<String, String>();
         String[] strs = string.split("&");
         for (String str : strs) {
-            String[] sts = new String[]{"",""};
+            String[] sts = new String[]{"", ""};
             String[] st = str.split("=");
-            if (st.length==1){
+            if (st.length == 1) {
                 sts[0] = st[0];
-            }else {
+            } else {
                 sts[0] = st[0];
                 sts[1] = st[1];
             }
-            stringMap.put(sts[0],sts[1]);
+            stringMap.put(sts[0], sts[1]);
         }
         return gson.toJson(stringMap);
     }
+
     /**
      * bean转json
+     *
      * @param string
      * @return
      */
-    public static String beanTojson(Object string){
+    public static String beanTojson(Object string) {
         return gson.toJson(string);
     }
+
     /**
      * bean转参数
+     *
      * @param object
      * @return
      */
-    public static String beanToParam(Object object){
+    public static String beanToParam(Object object) {
         return jsonToParam(gson.toJson(object));
     }
+
     /**
      * json转参数
+     *
      * @param string
      * @return
      */
-    public static String jsonToParam(String string){
-        Map<String,String> map = gson.fromJson(string, Map.class);
+    public static String jsonToParam(String string) {
+        Map<String, String> map = gson.fromJson(string, Map.class);
         String str = "";
         for (Map.Entry<String, String> entry : map.entrySet()) {
-            str+=entry.getKey()+"="+entry.getValue()+"&";
+            str += entry.getKey() + "=" + entry.getValue() + "&";
         }
-        str = str.substring(0,str.length()-1);
+        str = str.substring(0, str.length() - 1);
         return str;
     }
-   /**
+
+    /**
      * json转list
+     *
      * @param string
      * @return
      */
-    public static List<?> jsonToList(String string){
-        List<?> list= gson.fromJson(string, new TypeToken<List<?>>() {}.getType());
+    public static List<?> jsonToList(String string) {
+        List<?> list = gson.fromJson(string, new TypeToken<List<?>>() {
+        }.getType());
         return list;
     }
+
     /**
      * json转bean
+     *
      * @param string
      * @return
      */
-    public static Object jsonTobean(String string,Class<?> t){
-        return gson.fromJson(string,t);
+    public static Object jsonTobean(String string, Class<?> t) {
+        return gson.fromJson(string, t);
     }
+
     /**
      * json转bean
+     *
      * @param string
      * @return
      */
-    public static Object jsonTobean(String string, Type t){
-        return gson.fromJson(string,t);
+    public static Object jsonTobean(String string, Type t) {
+        return gson.fromJson(string, t);
     }
 }
