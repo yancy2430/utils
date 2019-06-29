2af04621ccdbf8866627498f636302850344a45b
杨哲 <2850755733@qq.com>
2019-06-29 10:37:34 +0800

    添加日志doc模块

---
+++ src/main/java/com/tdeado/utils/doc/IgnoreApi.java
@@ -0,0 +1,16 @@
+package com.tdeado.utils.doc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 忽略生成接口文档,标注了该注解的接口,将不会生成接口文档
+ * <p>
+ * Created by leaf on 2018/2/28.
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IgnoreApi {
+}
