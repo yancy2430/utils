2d6f3d942f9ae65af488500cd028284752a49dd0
yangzhe <yancy@tdeado.com>
2020-03-12 15:56:56 +0800

    添加扫描文件夹

--- src/main/java/com/tdeado/utils/io/FileUtil.java
+++ src/main/java/com/tdeado/utils/io/FileUtil.java
@@ -31,4 +31,62 @@ public class FileUtil {
         return true;
     }
 
+    /**
+     * TODO:递归扫描指定文件夹下面的指定文件
+     * @param folderPath 扫描目录
+     * @return ArrayList<Object>
+     */
+    public static ArrayList<Object> scanFilesWithRecursion(String folderPath) throws RuntimeException{
+        ArrayList<Object> scanFiles = new ArrayList<Object>();
+        ArrayList<String> dirctorys = new ArrayList<String>();
+        File directory = new File(folderPath);
+        if(!directory.isDirectory()){
+            throw new RuntimeException('"' + folderPath + '"' + " input path is not a Directory , please input the right path of the Directory. ^_^...^_^");
+        }
+        if(directory.isDirectory()){
+            File [] filelist = directory.listFiles();
+            for(int i = 0; i < filelist.length; i ++){
+                /**如果当前是文件夹，进入递归扫描文件夹**/
+                if(filelist[i].isDirectory()){
+                    dirctorys.add(filelist[i].getAbsolutePath());
+                    /**递归扫描下面的文件夹**/
+                    scanFiles.addAll(scanFilesWithRecursion(filelist[i].getAbsolutePath()));
+                }
+                /**非文件夹**/
+                else{
+                    scanFiles.add(filelist[i].getAbsolutePath());
+                }
+            }
+        }
+        return scanFiles;
+    }
+    /**
+     * TODO:递归扫描指定文件夹下面的指定文件
+     * @param folderPath 扫描目录
+     * @return ArrayList<Object>
+     */
+    public static ArrayList<File> scanFilesWithRecursion(File folderPath) throws RuntimeException{
+        ArrayList<File> scanFiles = new ArrayList<File>();
+        ArrayList<String> dirctorys = new ArrayList<String>();
+        File directory = folderPath;
+        if(!directory.isDirectory()){
+            throw new RuntimeException('"' + folderPath.getAbsolutePath() + '"' + " input path is not a Directory , please input the right path of the Directory. ^_^...^_^");
+        }
+        if(directory.isDirectory()){
+            File [] filelist = directory.listFiles();
+            for(int i = 0; i < filelist.length; i ++){
+                /**如果当前是文件夹，进入递归扫描文件夹**/
+                if(filelist[i].isDirectory()){
+                    dirctorys.add(filelist[i].getAbsolutePath());
+                    /**递归扫描下面的文件夹**/
+                    scanFiles.addAll(scanFilesWithRecursion(filelist[i]));
+                }
+                /**非文件夹**/
+                else{
+                    scanFiles.add(filelist[i]);
+                }
+            }
+        }
+        return scanFiles;
+    }
 }
